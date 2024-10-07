86ef2f4eab81f71245364d519511da0c01a3df65
Sushant Raipuri <cashu853@gmail.com>
2024-10-07 12:17:52 +0530

    added latest updates

--- js/contactscript.js
+++ js/contactscript.js
@@ -1,16 +1,21 @@
 function sendMsg() {
     let number = "+917876135383";
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let subject = document.getElementById('subject').value;
+    let message = document.getElementById('message').value;
 
-    let  name = document.getElementById('name').value;
-    let  email = document.getElementById('email').value;
-    let  subject = document.getElementById('subject').value;
-    let  message = document.getElementById('message').value;
+    // Validate if all fields are filled
+    if (!name || !email || !subject || !message) {
+        alert("Please fill in all fields before sending the message.");
+        return false; // Prevent opening WhatsApp URL if validation fails
+    }
 
-    var url = "https://wa.me/" +number+ "?text="
-    + "Name : " +name + "%0a"
-    + "E-mail : " +email + "%0a"
-    + "Subject : " +subject + "%0a"
-    + "Message : " +message + "%0a%0a";
+    var url = "https://wa.me/" + number + "?text="
+        + "Name: " + name + "%0a"
+        + "E-mail: " + email + "%0a"
+        + "Subject: " + subject + "%0a"
+        + "Message: " + message + "%0a%0a";
 
-    window.open(url, '_blank').focus()
+    window.open(url, '_blank').focus();
 }
